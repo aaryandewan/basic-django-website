8a4721ef085bdc88af0570613c8319866153eb0f
Aaryan Dewan <49852384+aaryandewan@users.noreply.github.com>
2019-12-13 10:30:48 +0530

    Added Delete and Edit functionalities

--- food/urls.py
+++ food/urls.py
@@ -1,6 +1,8 @@
 from . import views
 from django.urls import path
 
+app_name = 'food'
+
 urlpatterns = [
     path('', views.index, name = 'index'),
     path('item/', views.item, name = 'item'),
@@ -8,4 +10,8 @@ urlpatterns = [
     path('<int:item_id>/', views.detail, name = "detail" ),
     #add form
     path('add', views.create_item, name = "create"),
+    #edit item
+    path('update/<int:id>/', views.update_item, name = 'update_item'),
+    #delete item
+    path(r'^delete/<int:id>/', views.delete_item, name = 'delete_item')
 ]
